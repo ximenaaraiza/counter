d39cc65d3b75624b023fde30229bf25aab85a792
StudentAM <studentam@Administrators-MacBook-Pro-2.local>
2024-02-29 08:52:45 -0800

    countig

---
+++ counter/counter/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  counter
+//
+//  Created by StudentAM on 2/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var count : Int = 0
+    var body: some View {
+        VStack {
+            Text("Click buttons to change the counter ")
+            Text("\(count)")
+            HStack{
+                Button("Decrement",action: decrement)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                
+                Button("Increment",action:increment )
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        
+
+        }
+    func decrement(){
+        if count > 0 {
+            count -= 1
+        }
+    }
+
+    func increment(){
+        if count < 50{
+            count += 1
+        }
+    }
+    
+    }
+   
+
+
+
+#Preview {
+    ContentView()
+}
+
---
+++ counter/counter/counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterApp.swift
+//  counter
+//
+//  Created by StudentAM on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
